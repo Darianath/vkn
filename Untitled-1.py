d5784a5368a6abdf0c0c34b42a0cd318127d537d
Darianath <144663844+Darianath@users.noreply.github.com>
2023-09-29 23:26:33 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+while True:
+    path = input("Введіть шлях до файла: ")
+    if not path:
+        break
+    folders = path.split('\\')
+    print("Шлях до файла:")
+    for folder in folders[:-1]:
+        print(folder)
+    filename = folders[-1]
+    dot_index = filename.rfind('.')
+    if dot_index == -1:
+        name = filename
+        extension = ''
+    else:
+        name = filename[:dot_index]
+        extension = filename[dot_index+1:]
+    print(f'Назва файла: {name}, Розширення: {extension}')
